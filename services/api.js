bb739f87e9a3c9d314bc54e08edd9f23d0654bf2
João Queiroz <joao.queiroz.code@gmail.com>
2024-10-29 08:41:01 -0300

    Changes in Api

--- services/api.js
+++ services/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { API_BASE_URL } from '../constants/apiConfig';
 
 const api = axios.create({
@@ -9,8 +10,24 @@ const api = axios.create({
   },
 });
 
+api.interceptors.request.use(async (config) => {
+  const token = await AsyncStorage.getItem('authToken');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+const saveToken = async (token) => {
+  await AsyncStorage.setItem('authToken', token);
+};
+
 export const login = async (email, password) => {
   const response = await api.post('/api/login', { email, password });
+  const { token } = response.data;
+  await saveToken(token); // Salva o token
   return response.data;
 };
 
@@ -25,22 +42,27 @@ export const register = async (email, password) => {
 };
 
 export const getBooks = async () => {
-  const response = await api.get('/books');
+  const response = await api.get('/api/books');
   return response.data;
 };
 
 export const createBook = async (bookData) => {
-  const response = await api.post('/books', bookData);
-  return response.data;
+  try {
+    const response = await api.post('/api/books', bookData);
+    return response.data;
+  } catch (error) {
+    console.error("Error during registration:", error);
+    throw error;
+  }
 };
 
 export const updateBook = async (id, bookData) => {
-  const response = await api.put(`/books/${id}`, bookData);
+  const response = await api.put(`/api/books/${id}`, bookData);
   return response.data;
 };
 
 export const deleteBook = async (id) => {
-  const response = await api.delete(`/books/${id}`);
+  const response = await api.delete(`/api/books/${id}`);
   return response.data;
 };
 
